159103835b6235570b5c2d12b2ee4ffd0e253bb7
kreativetimebox <167294192+kreativetimebox@users.noreply.github.com>
2025-10-19 22:27:40 +0530

    Turn off 'react/no-unescaped-entities' rule
    Disable the 'react/no-unescaped-entities' rule in ESLint configuration.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,10 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+  // ...existing rules
+  'react/no-unescaped-entities': 'off',
+  },
     ignores: [
       "node_modules/**",
       ".next/**",
